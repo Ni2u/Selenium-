e452c090b923d66c373c58e441c2c3db04ff2421
Ni2u <75671441+Ni2u@users.noreply.github.com>
2021-01-04 20:29:42 +0100

    Add files via upload

---
+++ Login_automate.java
@@ -0,0 +1,35 @@
+package chrome_Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login_automate {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
+		WebDriver driverobject=new ChromeDriver();
+		driverobject.get("https://www.facebook.com/");
+		driverobject.findElement(By.name("email")).sendKeys("nithuusername");
+		driverobject.findElement(By.id("pass")).sendKeys("nithupassword");;
+		driverobject.findElement(By.id("u_0_h")).click();
+		driverobject.findElement(By.name("login")).click();
+		System.out.println("Current URL is " + driverobject.getCurrentUrl());
+		System.out.println("Page Title is " + driverobject.getTitle());
+		
+		try {
+			Thread.sleep(3000);
+		}
+		
+		catch(InterruptedException e)
+		{
+			System.out.println("User Interuppted");
+		}
+		
+		driverobject.close();
+		
+		
+	}
+
+}
